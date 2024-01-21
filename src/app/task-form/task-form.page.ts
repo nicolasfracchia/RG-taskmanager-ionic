4b0f2ec06b0ad5125f7a60317d6fd0ee1bb1f1e5
nicolas.fracchia <nicolas.fracchia@freshwatercreative.ca>
2024-01-20 17:13:33 -0700

    explore container removed

--- src/app/task-form/task-form.page.ts
+++ src/app/task-form/task-form.page.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-task-form',
   templateUrl: 'task-form.page.html',
   styleUrls: ['task-form.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
 })
 export class TaskFormPage {
 
